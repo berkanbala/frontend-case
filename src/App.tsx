ec94f3910ef24c7d65e7189f3daa52aea7380be2
berkan bala <berkan.balaa@gmail.com>
2024-02-26 23:45:06 +0300

    request

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,24 @@ import "./sel.scss";
 
 export default function CheckSelect() {
   const [selectedOptions, setSelectedOptions] = useState([] as any);
-  const handleChange = (value: any) => {
-    setSelectedOptions(value);
-  };
+
+  // const handleChange = (value: any, e: any) => {
+  //   setSelectedOptions(value);
+  //   console.log(e);
+  // };
 
   const [rickAndMortyOptions, setRickAndMortyOptions] = useState([] as any);
+
+  const handleChange = (value: string[]) => {
+    console.log(`selected ${value}`);
+    setSelectedOptions(value);
+  };
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character")
       .then((res) => res.json())
       .then((res) =>
+        // eslint-disable-next-line array-callback-return
         res.results.map((character: any) => {
           setRickAndMortyOptions((prevState: any) => [
             ...prevState,
@@ -33,19 +41,34 @@ export default function CheckSelect() {
 
   return (
     <div className="container">
-      {/* <Select onChange={(e: any) => console.log(e)} mode="multiple" open> */}
-      <Select onChange={(e: any) => console.log(e)} mode="multiple">
-        {rickAndMortyOptions.map((character: any) => (
-          <Select.Option key={character.value} label={character.value}>
+      <Select
+        value={selectedOptions}
+        onChange={handleChange}
+        mode="multiple"
+        allowClear
+        placeholder="Please select"
+        // open
+      >
+        {rickAndMortyOptions.map((character: any, index: any) => (
+          <Select.Option
+            key={index}
+            label={character.name}
+            value={character.value}
+          >
             <Checkbox
-            // checked={selectedOptions.includes("johhny")}
+              indeterminate
+              onChange={(e: any) =>
+                console.log(`checked = ${e.target.checked}`)
+              }
             >
               <span className="dropdown-value">{character.value}</span>
               <span>
                 <img src={character.details.image} alt="character img" />
               </span>
               <div className="rickandmorty-description">
-                <span>{character.value}</span>
+                <span className="rickandmorty-description-name">
+                  {character.value}
+                </span>
                 <span>{character.details.episodeCount} Episodes</span>
               </div>
             </Checkbox>
